aa397093a7d2acfca8a85f46e186a6d27b165497
kennethadriann <65903044+kennethadriann@users.noreply.github.com>
2023-02-01 07:24:55 +0800

    Update away.py

--- away.py
+++ away.py
@@ -1,26 +1,49 @@
-import win32api, win32con, random, time
-from pynput import keyboard
+import sys
+import logging
+import os
+import json
 import time
 
-def move(x,y):
-    win32api.SetCursorPos((x,y))
+import msal
 
-res_width = win32api.GetSystemMetrics(0)
-res_height = win32api.GetSystemMetrics(1)
+# Define your tenant ID and client ID
+TENANT_ID = "your_tenant_id"
+CLIENT_ID = "your_client_id"
 
-break_program = False
-def on_press(key):
-    global break_program
-    print (key)
-    if key == keyboard.Key.end:
-        print ('end pressed')
-        break_program = True
-        return False
+# Define the authority and scopes
+AUTHORITY = "https://login.microsoftonline.com/{}".format(TENANT_ID)
+SCOPES = ["https://graph.microsoft.com/.default"]
 
-with keyboard.Listener(on_press=on_press) as listener:
-    while break_program == False:
-        rand_x = random.randint(1,res_width/2)
-        rand_y = random.randint(1,res_height/2)
-        time.sleep(1)
-        move(rand_x,rand_y)
-    listener.join()
+# Create a callback to handle the redirect from Azure AD
+def acquire_token_callback(authority, scopes, username, password, client_id, client_credential, redirect_uri):
+    # Create the MSAL client
+    app = msal.PublicClientApplication(client_id, authority=authority)
+
+    # Acquire a token for the user
+    result = None
+    result = app.acquire_token_by_username_password(
+        username=username,
+        password=password,
+        scopes=scopes
+    )
+
+    # Check the result
+    if "access_token" in result:
+        print("Access token: ", result["access_token"])
+    else:
+        print("Error acquiring access token: ", result.get("error"))
+        print("Error description: ", result.get("error_description"))
+        print("Correlation ID: ", result.get("correlation_id"))
+
+# Main function
+def main():
+    # Get the username and password from environment variables
+    username = os.environ["AZURE_AD_USERNAME"]
+    password = os.environ["AZURE_AD_PASSWORD"]
+
+    # Call the function to acquire a token
+    acquire_token_callback(AUTHORITY, SCOPES, username, password, CLIENT_ID, None, None)
+
+# Call the main function
+if __name__ == "__main__":
+    main()
